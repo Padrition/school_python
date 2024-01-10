49e1d113f4bad9403e945abcf5a78012d8526ae8
Dmitrii Titarenko <padrition@gmail.com>
2024-01-10 18:47:41 +0100

    Sql rework

--- init_db.py
+++ init_db.py
@@ -5,9 +5,16 @@ con = sqlite3.connect("vwa.db")
 
 cur = con.cursor()
 
+with open("db.sql", 'r') as db_file:
+    sql_commands = db_file.read()
+    cur.executescript(sql_commands)
+
+con.commit()
+
 with open("vwa.sql", 'r') as sql_file:
     sql_commands = sql_file.read()
     cur.executescript(sql_commands)
 
 con.commit()
+
 con.close()
